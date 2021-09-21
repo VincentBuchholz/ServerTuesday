28fd84970a309087056e7d2af7fee1b1d8d1b365
Vincent Buchholz <vincbuchholz@gmail.com>
2021-09-21 09:39:41 +0200

    First commit

---
+++ src/CphBusiness/Main.java
@@ -0,0 +1,15 @@
+package CphBusiness;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	EchoServer echoServer = new EchoServer(8285);
+        try {
+            echoServer.startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
